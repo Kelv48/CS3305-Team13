33abf305dfbac1b29cbf1340209b05cd89952a5f
M-dok <145792723+M-dok@users.noreply.github.com>
2025-01-15 16:48:56 +0000

    An initial outline of main entities

---
+++ card.py
@@ -0,0 +1,37 @@
+from rank import Rank
+class Card():
+    def __init__(self, suit: str, rank: Rank):
+        '''
+        S: Spade
+        C: Clubs 
+        D: Diamonds
+        H: Hearts 
+        '''
+        self._suit = suit 
+        self._rank = rank 
+
+    def getSuit(self):
+        return self._suit
+    
+    def setSuit(self, suit):
+        self._suit = suit
+
+
+    def getRank(self):
+        return self._rank.value
+    
+    def setRank(self, rank: Rank):
+        self._rank = rank 
+
+    def __str__(self):
+        return str((self._suit, self._rank.name))
+
+    suit = property(getSuit, setSuit)
+    rank = property(getRank, setRank)
+
+if __name__ == "__main__":
+    der = Card("S",Rank.ACE)
+
+    print(der)
+
+    
