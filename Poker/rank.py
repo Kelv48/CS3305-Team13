49e549dce17c8afdd1bc6b858c83385230922ed1
M-dok <145792723+M-dok@users.noreply.github.com>
2025-01-19 21:12:16 +0000

    Removed duplicate code in rank.py and added to protocol.py

--- Poker/rank.py
+++ Poker/rank.py
@@ -14,22 +14,3 @@ class Rank(Enum):
     FOUR = 4
     THREE = 3
     TWO = 2
-    
-    #This is for the message headers between client and server 
-class Protocols:
-    class Response:
-        AUTHENTICATION_VALID = "protocol.valid_authentication"     #Used to responded with authentication credentials 
-        AUTHENTICATION_INVALID = "protocol.invalid_authentication" #Used to reject client login
-        QUESTIONS = "protocol.questions"
-        START = "protocol.start"
-        OPPONENT = "protocol.opponent"
-        OPPONENT_ADVANCE = 'protocol.opponent_advance'
-        ANSWER_VALID = "protocol.answer_valid"
-        ANSWER_INVALID = 'protocol.answer_invalid'
-        WINNER = "protocol.winner"
-        OPPONENT_LEFT = "protocol.opponent_left"
-
-    class Request:
-        RAISE = "protocol.raise"                                #Used by client to send request to raise the pot
-        AUTHENTICATION = "protocol.request_authentication"     #Used to request of auth when client logs in 
-        LEAVE = "protocol.leave"                                #Used to send request to leave game 
