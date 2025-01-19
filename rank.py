1d4a57fd784fd6e4c909a980276c84e99adb2e4a
M-dok <145792723+M-dok@users.noreply.github.com>
2025-01-19 20:50:11 +0000

    Implementing message types for client-server communication

--- rank.py
+++ rank.py
@@ -15,6 +15,19 @@ class Rank(Enum):
     THREE = 3
     TWO = 2
     
-#    def __str__(self):
-#        return str(self.value)
-    
+class Protocols:
+    class Response:
+        NICKNAME = "protocol.request_nickname"
+        QUESTIONS = "protocol.questions"
+        START = "protocol.start"
+        OPPONENT = "protocol.opponent"
+        OPPONENT_ADVANCE = 'protocol.opponent_advance'
+        ANSWER_VALID = "protocol.answer_valid"
+        ANSWER_INVALID = 'protocol.answer_invalid'
+        WINNER = "protocol.winner"
+        OPPONENT_LEFT = "protocol.opponent_left"
+
+    class Request:
+        ANSWER = "protocol.answer"
+        NICKNAME = "protocol.send_nickname"
+        LEAVE = "protocol.leave"
