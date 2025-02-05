69b28489ae2545ab718c175af4fb0b793c173545
Kelvin <136612032+Kelv48@users.noreply.github.com>
2025-02-05 12:29:05 +0000

    Add files via upload

---
+++ src/gui/guide.py
@@ -0,0 +1,37 @@
+import pygame, sys
+from src.gui.button import Button
+from src.gui.utils import BG, get_font, SCREEN
+
+def guide(mainMenu):
+    while True:
+        GUIDE_MOUSE_POS = pygame.mouse.get_pos()
+
+        # Calculate positions based on current screen size   # Scale the background to fit the screen
+        screen_width, screen_height = SCREEN.get_size()
+        scaled_bg = pygame.transform.scale(BG, (screen_width, screen_height))
+        SCREEN.blit(scaled_bg, (0, 0))
+
+        # Calculate positions based on current screen size
+        GUIDE_TEXT = get_font(45).render("This is the GUIDE screen.", True, "White")
+        GUIDE_RECT = GUIDE_TEXT.get_rect(center=(screen_width / 2, screen_height / 9))
+        SCREEN.blit(GUIDE_TEXT, GUIDE_RECT)
+
+        GUIDE_BACK = Button(
+            pos=(screen_width / 2, screen_height * 2 / 2.5), 
+            text_input="BACK", 
+            font=get_font(30), 
+            base_color="White", 
+            hovering_color="Light Green")
+
+        GUIDE_BACK.changeColor(GUIDE_MOUSE_POS)
+        GUIDE_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if GUIDE_BACK.checkForInput(GUIDE_MOUSE_POS):
+                    mainMenu()
+
+        pygame.display.update()
