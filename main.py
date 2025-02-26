ad7c8aa75a48ac1083d558d1cc240adb0c2bffbc
Yang <yuyaoyang2003@gmail.com>
2025-02-26 12:36:07 +0000

    Removed redundant features in single player.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.singleplayer_game.game_class import Game
+# from src.multiplayer_game.game_class import Game
 from src.gui.screens.main_menu import mainMenu
     
 if __name__ == "__main__":
